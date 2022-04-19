69127aa343d2db0596399afb6b7ef9d6662827f3
blitzschwert <43556248+blitzschwert@users.noreply.github.com>
2022-04-19 13:18:51 -0700

    Added serial functionality, created basic UI, created function for preset drawings

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
 import PySimpleGUI as sg
+import os
+import serial as s
 
+# Set the color theme for the GUI
 sg.theme('DarkAmber')
 
-layout = [  [sg.Text('Test')],
-            [sg.Button('Yes'), sg.Button('No')]  ]
+# Start up serial communication
+arm = s.Serial()
+arm.baudrate = 9600
+arm.port = 'COM1'
 
-main_window = sg.Window('Window Title', layout)
+# Create function for when the user selects a preset
+def presets(event):
+    # Create window for user
+    presets_layout = [[sg.Text('Please wait...')]]
+    presets_window = sg.Window('Sending Codes', presets_layout, disable_close = True)
+
+    # Read lines from the correct preset text file
+    with open(event) as f:
+        codes = f.readlines()
+    
+    # Open connection to arm
+    while arm.is_open() == False:
+        arm.open()
+
+    # Send codes to arm and wait between each
+    for code in codes:
+        arm.write(code)
+        wait(5)
+
+    presets_window.close()
+
+
+layout = [  [sg.Text('Pick an option')],
+            [sg.Text('Presets')],
+            [sg.Button('Line'), sg.Button('Square'), sg.Button('Circle'), sg.Button('Heart'), sg.Button('Star')],
+            [sg.Text('Custom')],
+            [sg.Button('Drawing'), sg.Button('Import Image'), sg.Button('Take Picture')]  ]
+
+main_window = sg.Window('Drawing Arm Control', layout)
 
 while True:
     event, values = main_window.read()
     if event == sg.WIN_CLOSED:
         break
+    elif event == 'Line' or event == 'Square' or event == 'Circle' or event == 'Heart' or event == 'Star':
+        presets(event)
 
 main_window.close()
