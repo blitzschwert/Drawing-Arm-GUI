5be0bddc155a8fdff1c8c6b799f51abec5992d18
blitzschwert <craigbrod7@gmail.com>
2022-05-28 15:59:38 -0700

    Updated to support all commands

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ sg.theme('DarkAmber')
 # Start up serial communication
 arm = s.Serial()
 arm.baudrate = 9600
-arm.port = 'COM4'
+arm.port = 'COM3'
 arm.timeout = 2
 
 # Create function for when the user selects a preset
@@ -70,7 +70,20 @@ def send_data():
             break
 
     # arm.close()
+    f.close()
     send_window.close()
+
+def send_command(command, x, y):
+    # arm.open()
+    time.sleep(2)
+    if command == 'G0' or command == 'G1' or command == 'G20' or command == 'G21' or command == 'G90' or command == 'G91':
+        command_to_send = command + ' X' + x + ' Y' + y
+        # arm.write(command_to_send.encode())
+        print(command_to_send)
+    elif command == 'M2' or command == 'M6' or command == 'M72':
+        # arm.write(command.encode())
+        print(command)
+
 
 # Create layout for main GUI
 layout = [  [sg.Text('Pick an option')],
@@ -81,7 +94,7 @@ layout = [  [sg.Text('Pick an option')],
             [sg.Text('Other Commands')],
             [sg.Button('G0'), sg.Button('G1'), sg.Button('G90'), sg.Button('G91'), sg.Button('G20')],
             [sg.Button('G21'), sg.Button('M2'), sg.Button('M6'), sg.Button('M72')],
-            [sg.Text('X'), sg.InputText(), sg.Text('Y'), sg.InputText()]  ]
+            [sg.Text('X'), sg.InputText(s = (5, 1)), sg.Text('Y'), sg.InputText(s = (5, 1))]  ]
 
 # Open GUI window
 main_window = sg.Window('Drawing Arm Control', layout)
@@ -102,6 +115,8 @@ while True:
     elif event == 'Take Picture':
         sp.call('python ./cvmain.py -p')
         send_data()
+    elif event == 'G0' or event == 'G1' or event == 'G90' or event == 'G91' or event == 'G20' or event == 'G21' or event == 'M2' or event == 'M6' or event == 'M72':
+        send_command(event, values[0], values[1])
 
 # Close window
 main_window.close()
